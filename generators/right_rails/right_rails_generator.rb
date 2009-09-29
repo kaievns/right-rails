1eafc213223d2cd7f790394bfccc3d161b30cf5c
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-09-29 12:16:39 +0400

    right_rails generator more correct directories handling

--- generators/right_rails/right_rails_generator.rb
+++ generators/right_rails/right_rails_generator.rb
@@ -5,18 +5,12 @@
 class RightRailsGenerator < Rails::Generator::Base
   
   mandatory_options :source => "#{File.dirname(__FILE__)}/../../javascripts"
-  
-  def initialize(args, options={})
-    ['public/javascripts/right', 'public/javascripts/right/i18n'].each do |directory|
-      Dir.mkdir directory unless File.directory?(directory)
-    end
     
-    super
-  end
-  
-  
   def manifest
     record do |m|
+      m.directory 'public/javascripts/right'
+      m.directory 'public/javascripts/right/i18n'
+      
       Dir.open(options[:source]).each do |file|
         unless ['.', '..'].include?(file)
           destination = if ['right.js', 'right-src.js'].include?(file)
